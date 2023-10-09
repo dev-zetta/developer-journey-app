57d580e8331588caab153443449a18461acf13b6
Gabriel Max <dev.zetta@gmail.com>
2023-10-09 09:55:56 +0200

    fix

--- src/components/sign-in-recommendation.tsx
+++ src/components/sign-in-recommendation.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ArrowUpIcon } from "@heroicons/react/24/outline";
+// import { ArrowUpIcon } from "@heroicons/react/24/outline";
 
 export default function SignInRecommendation() {
   return (
