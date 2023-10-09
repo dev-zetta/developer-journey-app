fe9a4a972e09cc5c9d5194e139840fcadd5e7b27
Gabriel Max <dev.zetta@gmail.com>
2023-10-09 09:53:12 +0200

    text edit

--- src/components/sign-in-recommendation.tsx
+++ src/components/sign-in-recommendation.tsx
@@ -18,10 +18,10 @@ import { ArrowUpIcon } from "@heroicons/react/24/outline";
 export default function SignInRecommendation() {
   return (
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-80">
-      <h1 className="text-3xl font-bold text-slate-100 float-right text-right">
-        <ArrowUpIcon className="h-32 w-32 float-right" aria-hidden="true" />
-        <br />
+      <h1 className="text-3xl font-bold text-slate-100">
+        {/* <ArrowUpIcon className="h-32 w-32 float-right" aria-hidden="true" /> */}
         Domena na prodej / Domain for sale
+        <br />
         <br />
         Kontakt/Contact: mat3d.cz@gmail.com
       </h1>
